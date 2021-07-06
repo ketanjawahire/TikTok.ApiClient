37ccba69b68bb7bb2dd298e342c65ef885c57585
Ashwini Gupta <ashwini.g@deltax.com>
2021-07-06 14:44:14 +0530

    Fix Objective Enum

--- src/TikTok.ApiClient/Enums/ObjectiveType.cs
+++ src/TikTok.ApiClient/Enums/ObjectiveType.cs
@@ -2,33 +2,64 @@
 {
     /// <summary>
     /// Objective Type enum
+    /// See - https://ads.tiktok.com/marketing_api/docs?id=1701890985340929
     /// </summary>
     public enum ObjectiveType
     {
-
         /// <summary>
         /// Null Objective Type
         /// </summary>
         NONE,
 
         /// <summary>
-        /// traffic
-        /// </summary>
-        TRAFFIC,
-
-        /// <summary>
-        /// app install
+        /// Get more people to install your app.
         /// </summary>
         APP_INSTALL,
 
         /// <summary>
-        /// convertions
+        /// Drive valuable actions on your website or app.
         /// </summary>
         CONVERSIONS,
 
         /// <summary>
-        /// The video views
+        /// Send more people to a destination on your website or app.
         /// </summary>
-        VIDEO_VIEWS
+        TRAFFIC,
+
+        /// <summary>
+        /// Get more people to view your video content.
+        /// </summary>
+        VIDEO_VIEWS,
+
+        /// <summary>
+        /// Show your ad to the maximum number of people.
+        /// </summary>
+        REACH,
+
+        /// <summary>
+        /// Collect leads for your business or brand.
+        /// </summary>
+        LEAD_GENERATION,
+
+        /// <summary>
+        /// Reach and frequency buying
+        /// </summary>
+        RF_REACH,
+
+        /// <summary>
+        /// Get more follows or profile visits.
+        /// </summary>
+        ENGAGEMENT,
+
+        /// <summary>
+        /// DPA Ad%% attract users to buy your products.
+        /// </summary>
+        CATALOG_SALES,
+
+        /// <summary>
+        /// Get more people to view your video content in R&F ads.
+        /// NOTE: In Doc its RF_VIDEO_VIEWS but in API call coming as RF_VIDEO_VIEW
+        /// </summary>
+        RF_VIDEO_VIEW
     }
 }
