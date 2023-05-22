a4a79443a678ddb2884135e9ad93c234aeab77da
Rohith D <rohith.d@deltax.com>
2023-05-22 14:11:44 +0530

    Added new enum values --refs DX-36315

--- src/TikTok.ApiClient/Enums/OptimizeGoal.cs
+++ src/TikTok.ApiClient/Enums/OptimizeGoal.cs
@@ -57,6 +57,26 @@
         /// </summary>
         VALUE,
 
-        ENGAGED_VIEW
+        ENGAGED_VIEW,
+
+        /// <summary>
+        /// Live shopping room viewer retention. This goal is applicable to live shopping scenario. The corresponding optimization_event is LIVE_STAY_TIME
+        /// </summary>
+        MT_LIVE_ROOM,
+
+        /// <summary>
+        /// Product link clicks during a live session. This goal is applicable to live shopping scenario. The corresponding optimization_event is LIVE_CLICK_PRODUCT_ACTION
+        /// </summary>
+        PRODUCT_CLICK_IN_LIVE,
+
+        /// <summary>
+        /// 15-second views (Focused view). The number of times your video has been played at least 15 seconds, or received at least 1 engagement within 1 day of the user seeing a paid ad.
+        /// </summary>
+        ENGAGED_VIEW_FIFTEEN,
+
+        /// <summary>
+        /// Landing Page View. The user clicks and loads the landing page successfully. The corresponding optimization_event is LANDING_PAGE_VIEW.
+        /// </summary>
+        TRAFFIC_LANDING_PAGE_VIEW
     }
 }
